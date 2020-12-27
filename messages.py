674577c0772ce676f1f61ef4be53aefea52d4bf5
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2020-12-27 15:23:21 +0100

    scoring system now works

--- messages.py
+++ messages.py
@@ -6,3 +6,6 @@ against_paper_lose = "You lose! Paper beats Rock."
 
 against_scissors_win = "You win! Rock beats Scissors."
 against_scissors_lose = "You lose! Scissors beats Paper."
+
+win = [against_rock_win, against_paper_win, against_scissors_win]
+lose = [against_rock_lose, against_paper_lose, against_scissors_lose]
